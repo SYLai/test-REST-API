f05b409bb5c6db84aa797ea33456c883ac233f69
SY Lai <laisy96@gmail.com>
2020-12-11 01:40:23 +0800

    link to database

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 var app = require("express")();
 var mysql = require("mysql");
-w   
-const test = {
-    sender : 'senderId',
-    receiver : 'receiverId',
-    date : 'sentDate',
-    title : 'msgTitle',
-    message: 'msg'
+
+var con = mysql.createConnection( {
+    host : "localhost",
+    user : "root",
+    password : "",
+    database : "msgdb"
+})
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("connected to database");
+    //date = new Date();
+    //console.log(date.toISOString().slice(0,19).replace('T',' '));
+})
+
+//msg constructor
+var msg = {
+    sender : '',
+    receiver : '',
+    timeSent : '',
+    title : '',
+    message: ''
 };
 
-app.get("/", (req, res) => res.send(test));
+app.get("/", (req, res) => res.send(msg));
 app.listen(3000, () => console.log("Server listening on port 3000"));
