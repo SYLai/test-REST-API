676d7770bf1362a1cf5fbfbaa7888552ad44b495
SY Lai <laisy96@gmail.com>
2020-12-11 03:34:19 +0800

    controls added

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var app = require("express")();
+var bodyParser = require("body-parser");
 var mysql = require("mysql");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 var con = mysql.createConnection( {
     host : "localhost",
@@ -11,20 +15,56 @@ var con = mysql.createConnection( {
 con.connect(function(err) {
     if (err) throw err;
     console.log("connected to database");
-    
 })
 
-//msg constructor
+//msg 
 var msg = {
-    sender : '',
-    receiver : '',
-    timeSent : '',
-    title : '',
-    message: ''
+    sender : "",
+    recipient : "",
+    time_sent : "",
+    title : "",
+    messege : ""
 };
 
 //date = new Date();
 //console.log(date.toISOString().slice(0,19).replace('T',' '));
 
+//homepage
 app.get("/", (req, res) => res.send(msg));
+//list messages
+app.get("/messages", function (req, res) {
+    con.query("SELECT * FROM messages", function(err, result) {
+        if (err) throw err;
+        res.send(result);
+    });
+});
+//send messages
+app.post("/messages", function (req, res){
+    date = new Date();
+    //assign values to msg
+    msg = {
+        sender : req.body.sender,
+        recipient : req.body.recipient,
+        time_sent : date.toISOString().slice(0,19).replace('T',' '), //convert js date to mysql datetime format
+        title : req.body.title,
+        message : req.body.message
+    };
+
+    //create new table row
+    con.query("INSERT INTO messages SET ?", msg, function(err, result){
+        if (err) throw err;
+        res.send(result);
+    });
+    
+});
+
+//view message
+app.get("/messages/:msgId", function (req, res) {
+    con.query("SELECT * FROM messages WHERE id =?", [req.params.msgId], function(err, result){
+        if (err) throw err;
+        res.send(result);
+    })
+})
+
+//listening to port 3000
 app.listen(3000, () => console.log("Server listening on port 3000"));
